9556dfeba13e11853bcb8d7c549d225a93c52359
V Supreetha Pai <156332592+suprithapai@users.noreply.github.com>
2024-07-03 21:49:52 +0530

    Prog17.py

---
+++ Prog17.py
@@ -0,0 +1,50 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+
+def kruskal(n, edges):
+    mst = []
+    edges.sort(key=lambda x: x[2])
+    disjoint_set = DisjointSet(n)
+
+    for edge in edges:
+        u, v, weight = edge
+        if disjoint_set.find(u) != disjoint_set.find(v):
+            disjoint_set.union(u, v)
+            mst.append(edge)
+
+    return mst
+
+# Example usage
+n = 4  # Number of vertices
+edges = [
+    (0, 1, 10),
+    (0, 2, 6),
+    (0, 3, 5),
+    (1, 3, 15),
+    (2, 3, 4)
+]
+
+mst = kruskal(n, edges)
+print("Edges in the Minimum Spanning Tree:")
+for u, v, weight in mst:
+    print(f"{u} -- {v} == {weight}")
